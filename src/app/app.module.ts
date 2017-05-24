4ccde1fa559cb23fdb3140a9e64fc66a6f713d55
dizco <gabriel.bourgault@kiosoft.ca>
2017-05-23 21:46:24 -0400

    Hotfix wrong file paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
+import { HomeComponent } from './components/home.component';
 
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'test/:id', component: TestComponent },
-  { path: '**', component: PageNotFoundComponent }
+  /*{ path: 'test/:id', component: TestComponent },
+  { path: '**', component: PageNotFoundComponent }*/
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
---
+++ src/app/components/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-component',
+  templateUrl: '../html/home.component.html',
+  styleUrls: ['../css/home.component.css']
+})
+export class HomeComponent {
+  title = 'app works2!';
+}
