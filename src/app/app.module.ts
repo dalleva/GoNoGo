061de217735f536a1d12cdbd9fce3580c84c09dd
dizco <gabriel.bourgault@kiosoft.ca>
2017-05-23 22:08:07 -0400

    Redo app architecture and add TestCanvas and Home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './components/app.component';
-import { HomeComponent } from './components/home.component';
+import { AppComponent } from './app/app.component';
+import { HomeComponent } from './home/home.component';
+import { TestCanvasComponent } from './test-canvas/test-canvas.component';
 
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  /*{ path: 'test/:id', component: TestComponent },
-  { path: '**', component: PageNotFoundComponent }*/
+  { path: 'test/:id', component: TestCanvasComponent },
+  // { path: '**', component: PageNotFoundComponent }*/
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TestCanvasComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
